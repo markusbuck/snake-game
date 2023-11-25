d7ad9253bf12711e36348f64dbcfb23559cb97f3
Markus Buckwalter <u1343465@utah.edu>
2023-11-25 13:39:40 -0700

    Added the following:
    - Fixed the snake segment drawing
    - cleaned up code
    - added the powerup stroke
    - added the ellipses in snake drawer

--- SnakeGame/SnakeClient/WorldPanel.cs
+++ SnakeGame/SnakeClient/WorldPanel.cs
@@ -101,12 +101,14 @@ public class WorldPanel : ScrollView,IDrawable
     {
         PowerUp p = o as PowerUp;
         int width = 16;
-
+        canvas.StrokeSize = 5;
+        canvas.StrokeColor = Colors.Yellow;
         canvas.FillColor = Colors.Red;
 
         // Ellipses are drawn starting from the top-left corner.
         // So if we want the circle centered on the powerup's location, we have to offset it
         // by half its size to the left (-width/2) and up (-height/2)
+        canvas.DrawEllipse(-(width / 2), -(width / 2), width, width);
         canvas.FillEllipse(-(width / 2), -(width / 2), width, width);
 
     }
@@ -114,15 +116,12 @@ public class WorldPanel : ScrollView,IDrawable
     private void SnakeSegmentDrawer(object o, ICanvas canvas)
     {
         int segmentLength = Convert.ToInt32(o);
-        canvas.StrokeColor = Colors.Red;
+        canvas.StrokeColor = Colors.Blue;
+        canvas.FillColor = Colors.Blue;
         canvas.StrokeSize = 10;
-        //canvas.DrawLine(0, 0, 0, -segmentLength);
+        canvas.FillEllipse(-5, -5, 10, 10);
         canvas.DrawLine(0, 0, 0, -segmentLength);
-
-        //canvas.FontColor = Colors.White;
-        //canvas.FontSize = 18;
-        //canvas.Font = Font.Default;
-        //canvas.DrawString("Kevin:", 0, -segmentLength, 100,100,  HorizontalAlignment.Left, VerticalAlignment.Top);
+        canvas.FillEllipse(-5, -5 + -segmentLength, 10, 10);
     }
 
     private void WallDrawer(object o, ICanvas canvas)
@@ -144,7 +143,6 @@ public class WorldPanel : ScrollView,IDrawable
                 float y = (float)leftPoint.GetY() - (width / 2);
 
                 canvas.DrawImage(this.wall, x, y, width, width);
-                //Console.WriteLine(x + " " + y);
             }
         }
 
@@ -157,10 +155,8 @@ public class WorldPanel : ScrollView,IDrawable
                 float x = (float)topPoint.GetX() - (width / 2);
                 float y = (float)topPoint.GetY() - (width / 2) - (segment * width);
                 canvas.DrawImage(this.wall, x, y, width, width);
-                //Console.WriteLine(x + " " + y);
             }
         }
-        //canvas.DrawImage(this.wall, (float) -wall.p1.GetX() / 2, (float) -wall.p1.GetY() / 2, width, 50);
     }
 
 
@@ -171,10 +167,6 @@ public class WorldPanel : ScrollView,IDrawable
 
         // undo previous transformations from last frame
         canvas.ResetState();
-
-        // example code for how to draw
-        // (the image is not visible in the starter code)
-        //canvas.DrawImage(wall, 0, 0, wall.Width, wall.Height);
 
         lock(theWorld)
         {
@@ -203,76 +195,20 @@ public class WorldPanel : ScrollView,IDrawable
                 foreach (var snake in theWorld.Snakes.Values)
                 {
 
-                    for (int i = 0; i < snake.body.Count -1; i++)
+                    for (int i = 0; i < snake.body.Count - 1; i++)
                     {
-                        Vector2D p1 = snake.body[i + 1]; // tail
-                        Vector2D p2 = snake.body[i];     // tip
+                        Vector2D p1 = snake.body[i]; // tail
+                        Vector2D p2 = snake.body[i + 1];     // tip
                         Vector2D direction = snake.dir;
-                        double segmentLengthX = Math.Abs(p2.GetX() - p1.GetX());
-                        double segmentLengthY = Math.Abs(p2.GetY() - p1.GetY());
 
-                        if (segmentLengthX > segmentLengthY)
-                        {
-                            double rotation = (direction.GetX() > 0) ? -90 : 90;
-                            DrawObjectWithTransform(canvas, segmentLengthX, p1.GetX(), p1.GetY(), rotation, SnakeSegmentDrawer);
-                        }
-                        else
-                        {
-                            double rotation = (direction.GetY() > 0) ? 0 : -180;
-                            DrawObjectWithTransform(canvas, segmentLengthY, p1.GetX(), p1.GetY(), rotation, SnakeSegmentDrawer);
-
-                        }
+                        double segmentLength = (p2 - p1).Length();
+                        Vector2D segmentRotation = (p2 - p1);
+                        segmentRotation.Normalize();
+                        DrawObjectWithTransform(canvas, segmentLength, p1.GetX(), p1.GetY(), segmentRotation.ToAngle(), SnakeSegmentDrawer);
                     }
-
-                    //double segmentLength = 0;
-                    //double segmentDirection = 0;
-
-                    //Vector2D head = snake.body[1];
-                    //Vector2D tail = snake.body[0];
-
-                    //if (snake.dir.GetX() != 0.0)
-                    //{
-                    //    segmentLength = Math.Abs(head.GetX() - tail.GetX());
-
-                    //    if(snake.dir.GetX() > 0)
-                    //    {
-                    //        segmentDirection = -90;
-                    //    }
-                    //    else
-                    //    {
-                    //        segmentDirection = 90;
-                    //    }
-                    //}
-                    //else
-                    //{
-                    //    segmentLength = Math.Abs(head.GetY() - tail.GetY());
-
-                    //    if (snake.dir.GetY() > 0)
-                    //    {
-                    //        segmentDirection = 0;
-                    //    }
-                    //    else
-                    //    {
-                    //        segmentDirection = -180;
-                    //    }
-
-                    //}
-
-
-                    //foreach (var bodyPart in snake.body)
-                    //{
-                    //    Vector2D vector = new Vector2D(bodyPart.GetX(), bodyPart.GetY());
-                    //    vector.Normalize();
-                    //    segmentLength += bodyPart.Length();
-                    //    segmentDirection += vector.ToAngle();
-                    //}
-
-                    //float angle = Vector2D.AngleBetweenPoints(snake.body[0], snake.body[1]);
-                    double segmentX = snake.body[1].GetX();
-                    double segmentY = snake.body[1].GetY();
-                    //Console.WriteLine( "WorldPanel: " + segmentX + " " + segmentY);
-
-                    //DrawObjectWithTransform(canvas, segmentLength, segmentX, segmentY, segmentDirection, SnakeSegmentDrawer);
+                    double segmentX = snake.body.Last().GetX();
+                    double segmentY = snake.body.Last().GetY();
+                    
                     canvas.FontColor = Colors.White;
                     canvas.FontSize = 18;
                     canvas.Font = Font.Default;
